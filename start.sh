8ef1ae02a8eca0766fedfbc442355ed37512d6fa
Jack Park <jackpark@gmail.com>
2019-06-21 15:40:13 -0700

    fixed jar version

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -Xms1024M -Xmx4G -cp ./target/tqos-asr-dictionary-server-0.3.0-SNAPSHOT-jar-with-dependencies.jar org.topicquests.asr.dictionary.Main
+java -Xms1024M -Xmx4G -cp ./target/tqos-asr-dictionary-server-0.4.0-SNAPSHOT-jar-with-dependencies.jar org.topicquests.asr.dictionary.Main
--- stop.sh
+++ stop.sh
@@ -1,1 +1,1 @@
-java -cp ./target/tqos-asr-dictionary-server-0.3.0-SNAPSHOT-jar-with-dependencies.jar org.topicquests.asr.dictionary.Stopper
+java -cp ./target/tqos-asr-dictionary-server-0.4.0-SNAPSHOT-jar-with-dependencies.jar org.topicquests.asr.dictionary.Stopper
